55ec0d9423877c6f0ff72abb401844f7b805ac7d
Carlos Alberto Llano R <carlos_llano@hotmail.com>
2018-02-26 23:12:02 -0500

    Added mysql

---
+++ web/mysql/README.md
@@ -0,0 +1,6 @@
+Links
+=====
+https://www.digitalocean.com/community/tutorials/how-to-set-up-mysql-master-master-replication
+https://www.digitalocean.com/community/tutorials/how-to-set-up-master-slave-replication-in-mysql
+http://www.cfg.jovenclub.cu/gulcf/?p=577
+http://www.digitalvalley.com/blog/configurar-arquitectura-maestro-maestro-mysql-para-la-replicacion-de-datos/
---
+++ web/mysql/basic/vagrant/mysql.sh
@@ -0,0 +1,22 @@
+# Download and Install the Latest Updates for the OS
+apt-get update && apt-get upgrade -y
+
+# Set the Server Timezone to CST
+echo "America/Chicago" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+# Enable Ubuntu Firewall and allow SSH & MySQL Ports
+ufw enable
+ufw allow 22
+ufw allow 3306
+
+# Install essential packages
+apt-get -y install zsh htop
+
+echo "Installing MySQL 5.7 now..."
+
+LOGFILE=/tmp/install_log.txt
+
+echo "mysql-server-5.7 mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again password root" | sudo debconf-set-selections
+apt-get -y install mysql-server-5.7 mysql-client >> $LOGFILE 2>&1
