c309f9199eff9e1ea17a8fbec4c9321e8d8262d8
Carlos Alberto Llano R <carlos.llano@sourcemeridian.com>
2019-07-07 10:18:24 -0500

    added another example in rabbitmq

---
+++ messaging/rabbitmq/docker/rabbitmq_client/examples/direct_messages/send.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq_server'))
+channel = connection.channel()
+
+channel.queue_declare(queue='my_queue')
+
+channel.basic_publish(exchange='',
+                      routing_key='my_queue',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
