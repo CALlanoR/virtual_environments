89b6b50cf408cb3ee9d587dfee31e85626d0fd73
Carlos Alberto Llano R <carlos.llano@sourcemeridian.com>
2020-05-04 11:15:13 -0500

    added config file in consumer

--- web/api_rest/mini_facebook/python_publications_consumer/publications_consumer/consumer.py
+++ web/api_rest/mini_facebook/python_publications_consumer/publications_consumer/consumer.py
@@ -1,43 +1,47 @@
 from pymongo import MongoClient
+from configparser import ConfigParser
 import json
 import pika
 
 def _save_publication_in_database(publication):
-    client = MongoClient('mongodb://localhost:27017',
-                         username='root',
-                         password='example')
-    db = client['publicationsDB']
+    client = MongoClient(_get_property('mongodb','url'),
+                         username=_get_property('mongodb','username'),
+                         password=_get_property('mongodb','password'))
+    db = client[_get_property('mongodb','database')]
     result = db.publications.insert_one(publication)
     print('One post: {0}'.format(result.inserted_id))
 
 def main():
-    credentials = pika.PlainCredentials('rabbitmq',
-                                        'rabbitmq')
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', 
-                                                                    5673, 
-                                                                    '/', 
-                                                                    credentials))
+    credentials = pika.PlainCredentials(_get_property('rabbitmq','user'),
+                                        _get_property('rabbitmq','password'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(_get_property('rabbitmq','url'),
+                                                                   int(_get_property('rabbitmq','port')),
+                                                                   '/',
+                                                                   credentials))
     channel = connection.channel()
-    channel.exchange_declare(exchange='new_publication_event',
-                            exchange_type='fanout')
+    channel.exchange_declare(exchange=_get_property('rabbitmq','exchange'),
+                             exchange_type='fanout')
 
     result = channel.queue_declare(queue='',
-                                exclusive=True)
+                                   exclusive=True)
     queue_name = result.method.queue
-
-    channel.queue_bind(exchange='new_publication_event',
-                    queue=queue_name)
+    channel.queue_bind(exchange=_get_property('rabbitmq','exchange'),
+                       queue=queue_name)
 
     def callback(ch, method, properties, body):
         publication = json.loads(body)
         _save_publication_in_database(publication)
 
     channel.basic_consume(queue=queue_name,
-                        on_message_callback=callback,
-                        auto_ack=True)
+                          on_message_callback=callback,
+                          auto_ack=True)
 
     channel.start_consuming()
 
+def _get_property(context, property_name):
+    config = ConfigParser()
+    config.read('config.ini')
+    return config.get(context, property_name)
+
 if __name__ == "__main__":
     main()
-
